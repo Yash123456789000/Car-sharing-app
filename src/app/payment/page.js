902a590badac3f3e137160132229d01c3a3c12ef
Yash123456789000 <iamyash@kgpian.iitkgp.ac.in>
2025-02-24 21:54:38 +0530

    4th message

--- src/app/payment/page.js
+++ src/app/payment/page.js
@@ -1,27 +1,10 @@
-"use client"
-import CheckoutForm from '../../components/Home/CheckoutForm';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-import { useSearchParams } from 'next/navigation'
-import React, { useContext } from 'react'
-import { FareUpdateContext } from '../../context/FareUpdateContext';
-import { Toaster } from "../../components/ui/sonner"
+import { Suspense } from "react";
+import Payment from "../../components/payment";
 
-const Payment = () => {
-    const searchParams=useSearchParams();
-    const amount=searchParams.get('amount');
-    const stripePromise=loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
-    const options={
-        mode: 'payment',
-        amount: Math.round(amount*100),
-        currency: 'usd',
-    }
-    const {fareUpdated, setFareUpdated} = useContext(FareUpdateContext)
+export default function PaymentPage() {
   return (
-    <Elements stripe={stripePromise} options={options}>
-        {fareUpdated?<><CheckoutForm amount={amount}/><Toaster /></>:<CheckoutForm amount={amount}/>}
-    </Elements>
-  )
+    <Suspense fallback={<div>Loading...</div>}>
+      <Payment />
+    </Suspense>
+  );
 }
-
-export default Payment
